48b81f933f8fc03caa18e47ba3c17711004b8ef3
mudevi <mudevi@gmail.com>
2017-04-21 22:48:14 -0400

    sticky header

--- wp-content/themes/acme-fx/functions.php
+++ wp-content/themes/acme-fx/functions.php
@@ -54,6 +54,12 @@ function genesis_sample_enqueue_scripts_styles() {
 
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+
+	wp_enqueue_script( 'genesis-sample-global', get_stylesheet_directory_uri() . '/js/global.js', array( 'jquery', 'fitvids' ), CHILD_THEME_VERSION );
+	wp_enqueue_script( 'genesis-sample-headhesive', get_stylesheet_directory_uri() . '/js/headhesive.min.js', array( 'jquery' ), CHILD_THEME_VERSION );
+	wp_enqueue_script( 'fitvids', get_stylesheet_directory_uri() . '/js/jquery.fitvids.js', array( 'jquery' ), CHILD_THEME_VERSION );
+
+
 	wp_localize_script(
 		'genesis-sample-responsive-menu',
 		'genesis_responsive_menu',
@@ -61,6 +67,7 @@ function genesis_sample_enqueue_scripts_styles() {
 	);
 
 }
+
 
 // Define our responsive menu settings.
 function genesis_sample_responsive_menu_settings() {
@@ -94,8 +101,8 @@ add_theme_support( 'genesis-responsive-viewport' );
 
 // Add support for custom header.
 add_theme_support( 'custom-header', array(
-	'width'           => 600,
-	'height'          => 160,
+	'width'           => 1800,
+	'height'          => 600,
 	'header-selector' => '.site-title a',
 	'header-text'     => false,
 	'flex-height'     => true,
@@ -116,25 +123,25 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 add_image_size( 'featured-image', 720, 400, TRUE );
 
 // Rename primary and secondary navigation menus.
-add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+// add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
 
 // Reposition the secondary navigation menu.
-remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
+// remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+// add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
 
 // Reduce the secondary navigation menu to one level depth.
-add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
-function genesis_sample_secondary_menu_args( $args ) {
+// add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+// function genesis_sample_secondary_menu_args( $args ) {
 
-	if ( 'secondary' != $args['theme_location'] ) {
-		return $args;
-	}
+// 	if ( 'secondary' != $args['theme_location'] ) {
+// 		return $args;
+// 	}
 
-	$args['depth'] = 1;
+// 	$args['depth'] = 1;
 
-	return $args;
+// 	return $args;
 
-}
+// }
 
 // Modify size of the Gravatar in the author box.
 add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
@@ -162,3 +169,18 @@ function my_header_video_settings( $settings ) {
   return $settings;
 }
 
+
+
+// Rename primary, secondary and tertiary navigation menus
+// add_theme_support ( 'genesis-menus' , array (
+// 	'primary' 	=> __( 'Above Header Menu', 'genesis-sample' ),
+// 	'secondary' => __( 'Below Header Menu', 'genesis-sample' ),
+//   'tertiary'  => __( 'Footer Menu', 'genesis-sample' )
+// ) );
+
+//* Reposition the primary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_before_header', 'genesis_do_nav', 5 );
+
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_header', 'genesis_do_subnav' );
