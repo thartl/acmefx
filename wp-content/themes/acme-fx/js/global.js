7c619ad6eeaf4971c212ffd3cdfa656f23963956
mudevi <mudevi@gmail.com>
2017-04-22 21:15:46 -0400

    tomas pull

--- wp-content/themes/acme-fx/js/global.js
+++ wp-content/themes/acme-fx/js/global.js
@@ -4,11 +4,12 @@ jQuery( function ( $ ) {
 
 	// Sticky Header options
 	var options = {
-			offset: 860
+			offset: 865
 	};
 
 	// Initialize with options
 	var header = new Headhesive('.nav-secondary', options);
-	var header = new Headhesive('.header-widget-area', 'offset: 920');
+	var header = new Headhesive('.header-widget-area', 'offset: 865');
+	//var header = new Headhesive('.title-area', 'offset: 865');
 
 });
