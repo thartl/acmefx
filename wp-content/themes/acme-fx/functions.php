ee67a62fb41145463e61bca989c4d3aa7692cde2
Tomas Hartl <lachtani@gmail.com>
2017-04-26 18:32:26 -0400

    adding 3 files...

---
+++ wp-content/themes/acme-fx/functions.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Acme FX.
+ *
+ * This file adds functions to the Acme FX Theme.
+ *
+ * @package Acme FX
+ * @author  Parkdale Wire
+ * @license GPL-2.0+
+ * @link    http://www.studiopress.com/
+ */
+
+// Start the engine.
+include_once( get_template_directory() . '/lib/init.php' );
+
+// Setup Theme.
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+
+// Set Localization (do not remove).
+add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
+function genesis_sample_localization_setup(){
+	load_child_theme_textdomain( 'genesis-sample', get_stylesheet_directory() . '/languages' );
+}
+
+// Add the helper functions.
+include_once( get_stylesheet_directory() . '/lib/helper-functions.php' );
+
+// Add Image upload and Color select to WordPress Theme Customizer.
+require_once( get_stylesheet_directory() . '/lib/customize.php' );
+
+// Include Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+// Add WooCommerce support.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php' );
+
+// Add the required WooCommerce styles and Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php' );
+
+// Add the Genesis Connect WooCommerce notice.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php' );
+
+// Child theme (do not remove).
+define( 'CHILD_THEME_NAME', 'Acme FX' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '1.0.1' );
+
+// Enqueue Scripts and Styles.
+add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
+function genesis_sample_enqueue_scripts_styles() {
+
+	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'dashicons' );
+
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . "/js/responsive-menus.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+
+	wp_enqueue_script( 'genesis-sample-global', get_stylesheet_directory_uri() . '/js/global.js', array( 'jquery' ), CHILD_THEME_VERSION );
+	wp_enqueue_script( 'genesis-sample-headhesive', get_stylesheet_directory_uri() . '/js/headhesive.min.js', array( 'jquery' ), CHILD_THEME_VERSION );
+
+	wp_localize_script(
+		'genesis-sample-responsive-menu',
+		'genesis_responsive_menu',
+		genesis_sample_responsive_menu_settings()
+	);
+
+}
+
+
+// Define our responsive menu settings.
+function genesis_sample_responsive_menu_settings() {
+
+	$settings = array(
+		'mainMenu'          => __( 'Menu', 'genesis-sample' ),
+		'menuIconClass'     => 'dashicons-before dashicons-menu',
+		'subMenu'           => __( 'Submenu', 'genesis-sample' ),
+		'subMenuIconsClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'       => array(
+			'combine' => array(
+				'.nav-primary',
+				'.nav-header',
+			),
+			'others'  => array(),
+		),
+	);
+
+	return $settings;
+
+}
+
+// Add HTML5 markup structure.
+add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+
+// Add Accessibility support.
+add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+
+// Add viewport meta tag for mobile browsers.
+add_theme_support( 'genesis-responsive-viewport' );
+
+// Add support for custom header.
+add_theme_support( 'custom-header', array(
+	'width'           => 1800,
+	'height'          => 600,
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+	'flex-height'     => true,
+	'video' => true,
+) );
+add_action( 'genesis_header', 'the_custom_header_markup' );
+
+// Add support for custom background.
+add_theme_support( 'custom-background' );
+
+// Add support for after entry widget.
+add_theme_support( 'genesis-after-entry-widget-area' );
+
+// Add support for 3-column footer widgets.
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+// Add Image Sizes.
+add_image_size( 'featured-image', 720, 400, TRUE );
+
+// Rename primary and secondary navigation menus.
+// add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+
+// Reposition the secondary navigation menu.
+// remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+// add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
+
+// Reduce the secondary navigation menu to one level depth.
+// add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+// function genesis_sample_secondary_menu_args( $args ) {
+
+// 	if ( 'secondary' != $args['theme_location'] ) {
+// 		return $args;
+// 	}
+
+// 	$args['depth'] = 1;
+
+// 	return $args;
+
+// }
+
+// Modify size of the Gravatar in the author box.
+add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+function genesis_sample_author_box_gravatar( $size ) {
+	return 90;
+}
+
+// Modify size of the Gravatar in the entry comments.
+add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+function genesis_sample_comments_gravatar( $args ) {
+
+	$args['avatar_size'] = 60;
+
+	return $args;
+
+}
+add_filter( 'header_video_settings', 'my_header_video_settings');
+function my_header_video_settings( $settings ) {
+  $settings['l10n'] = array(
+    'pause'      => __( '<span class="dashicons dashicons-controls-pause"></span>' ),
+    'play'       => __( '<span class="dashicons dashicons-controls-play"></span>' ),
+    'pauseSpeak' => __( 'Video stopped.'),
+    'playSpeak'  => __( 'Video started.'),
+  );
+  return $settings;
+}
+
+
+
+// Rename primary, secondary and tertiary navigation menus
+// add_theme_support ( 'genesis-menus' , array (
+// 	'primary' 	=> __( 'Above Header Menu', 'genesis-sample' ),
+// 	'secondary' => __( 'Below Header Menu', 'genesis-sample' ),
+//   'tertiary'  => __( 'Footer Menu', 'genesis-sample' )
+// ) );
+
+//* Reposition the primary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_before_header', 'genesis_do_nav', 5 );
+
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_header', 'genesis_do_subnav' );
---
+++ wp-content/themes/acme-fx/js/global.js
@@ -0,0 +1,13 @@
+jQuery( function ( $ ) {
+	
+	// Sticky Header options
+	var options = {
+			offset: 865
+	};
+
+	// Initialize with options
+	var header = new Headhesive('.nav-secondary', options);
+	var header = new Headhesive('.header-widget-area', 'offset: 865');
+	//var header = new Headhesive('.title-area', 'offset: 865');
+
+});
