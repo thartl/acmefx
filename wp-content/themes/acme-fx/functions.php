b57c1a3d27adc55195fd9fa008b3285df38d3fda
Tomas Hartl <lachtani@gmail.com>
2017-04-29 21:35:43 -0400

    non-change

--- wp-content/themes/acme-fx/functions.php
+++ wp-content/themes/acme-fx/functions.php
@@ -126,12 +126,9 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 add_image_size( 'featured-image', 720, 400, TRUE );
 
 // Rename primary and secondary navigation menus.
-add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
 // add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
 
 // Reposition the secondary navigation menu.
-remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
 // remove_action( 'genesis_after_header', 'genesis_do_subnav' );
 // add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
 
