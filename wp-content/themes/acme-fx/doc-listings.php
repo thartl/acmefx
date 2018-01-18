917c75558e8adb1bdd48fd8c5639c7bf6c46c1a7
Tomas Hartl <lachtani@gmail.com>
2018-01-18 10:59:46 -0500

    doc listings pages: don't print 0 if year missing, style info after year, only show 2 decimal places if file larger than 1MB

--- wp-content/themes/acme-fx/doc-listings.php
+++ wp-content/themes/acme-fx/doc-listings.php
@@ -15,7 +15,7 @@ function th_doc_repeater() {
 
 	if( $docs ) {
 
-		echo '<ul class="">';
+		echo '<ul class="doc-listings">';
 
 		for( $i = 0; $i < $docs; $i++ ) {
 
@@ -23,6 +23,8 @@ function th_doc_repeater() {
 			$doc_id = (int) get_post_meta( get_the_ID(), 'doc_info_' . $i . '_url', true );
 			$year = (int) get_post_meta( get_the_ID(), 'doc_info_' . $i . '_year', true );
 			$pages = (int)  get_post_meta( get_the_ID(), 'doc_info_' . $i . '_pages', true );
+
+			$maybe_year_with_middot = $year > 0 ? '<span class="doc-year">' . $year . '</span>&nbsp; <span class="pages-and-size">&middot; &nbsp;</span>' : '';
 
 			$url = esc_url( wp_get_attachment_url( $doc_id ) );
 			$url_prepend = get_site_url() . '/wp-content/plugins/pdfjs-viewer-shortcode/pdfjs/web/viewer.php?file=';
@@ -33,9 +35,10 @@ function th_doc_repeater() {
 			$page_s = $pages > 1 ? ' pages' : ' page';
 
 			$file_size = filesize( get_attached_file( $doc_id ) );
-			$file_size_nice = size_format( $file_size, 2 );
+			// Human readable, while showing 2 decimal places for files larger than 1MB
+			$file_size_nice = $file_size > 1048576 ? size_format( $file_size, 2 ) : size_format( $file_size, 0 );
 
-			echo '<li><a href="' . $pdf_js_url_complete . '" target="_blank">' . $title . '</a><p><span class="doc-year">' . $year . '</span>&nbsp; &middot; &nbsp;' . $pages . $page_s . '&nbsp; &middot; &nbsp;<span class="doc-file-size">' . $file_size_nice . '</span></p></li>';
+			echo '<li><a href="' . $pdf_js_url_complete . '" target="_blank">' . $title . '</a><p>' . $maybe_year_with_middot . '<span class="pages-and-size">' . $pages . $page_s . '&nbsp; &middot; &nbsp;' . $file_size_nice . '</span></p></li>';
 
 		}
 
